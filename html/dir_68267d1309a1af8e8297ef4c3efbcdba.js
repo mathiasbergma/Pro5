f75e623487d17787b3f2e07701a1925a52778eae
Mathias Berglund Mathiesen <you@example.com>
2022-12-15 21:21:14 +0100

    Updated doxyfiles

--- html/dir_68267d1309a1af8e8297ef4c3efbcdba.js
+++ html/dir_68267d1309a1af8e8297ef4c3efbcdba.js
@@ -1,6 +1,5 @@
 var dir_68267d1309a1af8e8297ef4c3efbcdba =
 [
     [ "AT_commands.h", "_a_t__commands_8h.html", null ],
-    [ "certs.h", "certs_8h_source.html", null ],
     [ "NB_R410M.h", "_n_b___r410_m_8h.html", "_n_b___r410_m_8h" ]
 ];
